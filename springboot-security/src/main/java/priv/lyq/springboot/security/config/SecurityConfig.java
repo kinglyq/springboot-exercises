d6090139f5e2d217ab5f8157cee31947983e3cc2
kinglyq <755365600@qq.com>
2020-07-21 21:26:17 +0800

    丰富配置，使用lambda表达式作为configure的写法

--- springboot-security/src/main/java/priv/lyq/springboot/security/config/SecurityConfig.java
+++ springboot-security/src/main/java/priv/lyq/springboot/security/config/SecurityConfig.java
@@ -4,21 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import priv.lyq.springboot.security.filter.JwtAuthenticationEntryPoint;
-import priv.lyq.springboot.security.filter.JwtAuthenticationFilter;
-import priv.lyq.springboot.security.filter.JwtAuthorizationFilter;
+import priv.lyq.springboot.security.access.LoginAuthenticationEntryPoint;
+import priv.lyq.springboot.security.authentication.AuthenticationFilter;
+import priv.lyq.springboot.security.authentication.AuthorizationFilter;
+import priv.lyq.springboot.security.access.AuthenticationDeniedHandler;
 
 import javax.annotation.Resource;
 
 /**
+ * Spring Security配置
+ *
  * @author Li Yuqing
+ * @date 2020-05-24 09:33:00
  */
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -32,7 +38,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService);
        /* auth.inMemoryAuthentication() //认证信息存储到内存中
                 .passwordEncoder(passwordEncoder())
-                .withUser("user").password(passwordEncoder().encode("123456")).roles("sdfsdf");*/
+                .withUser("user").password(passwordEncoder().encode("123456")).roles("admin");*/
+    }
+
+    /**
+     * 忽略静态资源
+     *
+     * @param web {@link WebSecurity}
+     */
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/assets/**");
     }
 
     /**
@@ -40,29 +56,57 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // 跨域共享
-        http.cors()
-                .and()
-                // 跨域伪造请求限制无效
-                .csrf().disable()
-                .authorizeRequests()
-                // 访问/data需要admin角色
-                // .antMatchers("/data").hasAnyRole()
-                // 其余资源任何人都可访问
-                .anyRequest().permitAll()
-                .and()
-                // 添加JWT登录拦截器
-                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
-                // 添加JWT鉴权拦截器
-                .addFilter(new JwtAuthorizationFilter(authenticationManager()))
-                .sessionManagement()
-                // 设置Session的创建策略为：Spring Security永不创建HttpSession 不使用HttpSession来获取SecurityContext
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+        // 排除的接口
+        String[] permits = {
+                "/login", "/open/**", "/h2/**"
+        };
+        http
+                // 登录页面相关
+                /* .formLogin(formLogin -> formLogin
+                         .loginProcessingUrl("/login.html")
+                         .usernameParameter("username")
+                         .passwordParameter("password")
+                         .permitAll()
+                 )*/
+                // 权限设置相关
+                .authorizeRequests(authorize -> authorize
+                                // authorTest 接口需要 admin_test 角色
+                                // .antMatchers("/authorTest").hasRole("admin_test")
+                                // permits下的请求任何人都可访问
+                                .antMatchers(permits).permitAll()
+                                // 未排除的所有请求必须经过身份验证方可使用
+                                .anyRequest().authenticated()
+                        // 所有请求可访问
+                        // .anyRequest().permitAll()
+                )
+                // 登出相关
+                /*.logout(logout -> logout
+                                // 登出接口
+                                .logoutUrl("/logout")
+                        // .addLogoutHandler()
+                )*/
+                // 添加 JWT 登录拦截器
+                .addFilter(new AuthenticationFilter(authenticationManager()))
+                // 添加 JWT 鉴权拦截器
+                .addFilter(new AuthorizationFilter(authenticationManager()))
+                // session相关
+                .sessionManagement(session -> session
+                        // 设置Session的创建策略为：Spring Security永不创建HttpSession 不使用HttpSession来获取SecurityContext
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                // CSRF请求限制无效
+                .csrf(AbstractHttpConfigurer::disable)
                 // 异常处理
                 .exceptionHandling()
                 // 匿名用户访问无权限资源时的异常
-                .authenticationEntryPoint(new JwtAuthenticationEntryPoint());
+                .authenticationEntryPoint(new LoginAuthenticationEntryPoint())
+                // 鉴权失败处理
+                .accessDeniedHandler(new AuthenticationDeniedHandler())
+                .and()
+                .headers(header -> {
+                    // 防止打不开h2控制台页面
+                    header.frameOptions().disable();
+                });
     }
 
     /**
