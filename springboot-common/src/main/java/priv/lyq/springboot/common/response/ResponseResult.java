522d4ca5d57461fd0b4b17f30ec7b936f91c226f
kinglyq <755365600@qq.com>
2020-07-27 22:38:39 +0800

    调整返回参数，添加注释

--- springboot-common/src/main/java/priv/lyq/springboot/common/response/ResponseResult.java
+++ springboot-common/src/main/java/priv/lyq/springboot/common/response/ResponseResult.java
@@ -1,5 +1,7 @@
 package priv.lyq.springboot.common.response;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.HashMap;
 
 /**
@@ -12,24 +14,48 @@ public class ResponseResult extends HashMap<String, Object> {
 
     public static final int INITIAL_CAPACITY = 16;
 
+    /**
+     * 业务处理成功
+     *
+     * @return {@link ResponseResult}
+     */
     public static ResponseResult success() {
         return ResponseResult.success(null);
     }
 
+    /**
+     * 业务处理成功，使用{@code ResponseStatus.OK}的reasonPhrase作msg，不接受自定义
+     *
+     * @param data 具体数据
+     * @return {@link ResponseResult}
+     */
     public static ResponseResult success(Object data) {
-        final ResponseStatus ok = ResponseStatus.OK;
+        ResponseStatus ok = ResponseStatus.OK;
         return new ResponseResult(ok.code, ok.reasonPhrase, data);
     }
 
+    /**
+     * 业务处理错误，msg默认为{@code ResponseStatus}的reasonPhrase
+     *
+     * @param status {@link ResponseStatus}
+     * @return {@link ResponseResult}
+     */
     public static ResponseResult error(ResponseStatus status) {
         return new ResponseResult(status.code, status.reasonPhrase, null);
     }
 
-    public static ResponseResult error(ResponseStatus status, Object data) {
-        return new ResponseResult(status.code, status.reasonPhrase, data);
-    }
-
+    /**
+     * 业务处理错误
+     *
+     * @param status {@link ResponseStatus}
+     * @param msg    错误提示信息，不写默认为{@code ResponseStatus}的reasonPhrase
+     * @param data   没啥放的可以放出错的堆栈信息
+     * @return {@link ResponseResult}
+     */
     public static ResponseResult error(ResponseStatus status, String msg, Object data) {
+        if(StringUtils.isBlank(msg)){
+            msg = status.reasonPhrase;
+        }
         return new ResponseResult(status.code, msg, data);
     }
 
