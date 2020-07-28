979efb9df43f13db81f1ceacea88f3c5b54fcaa0
kinglyq <755365600@qq.com>
2020-07-28 22:51:51 +0800

    使用google验证码

---
+++ springboot-security/src/main/java/priv/lyq/springboot/security/config/KaptchaConfig.java
@@ -0,0 +1,41 @@
+package priv.lyq.springboot.security.config;
+
+import com.google.code.kaptcha.Constants;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+/**
+ * kaptcha验证码配置
+ *
+ * @author Li Yuqing
+ * @date 2020-07-28 16:38:29
+ */
+@Configuration
+public class KaptchaConfig {
+
+    @Bean("kaptcha")
+    public DefaultKaptcha kaptcha() {
+        DefaultKaptcha kaptcha = new DefaultKaptcha();
+        Properties properties = new Properties();
+        properties.setProperty(Constants.KAPTCHA_BORDER, "yes");
+        properties.setProperty(Constants.KAPTCHA_BORDER_COLOR, "220,220,220");
+        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_FONT_COLOR, "38,29,12");
+        properties.setProperty(Constants.KAPTCHA_IMAGE_WIDTH, "120");
+        properties.setProperty(Constants.KAPTCHA_IMAGE_HEIGHT, "35");
+        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_FONT_SIZE, "25");
+        properties.setProperty(Constants.KAPTCHA_SESSION_KEY, "code");
+        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_CHAR_LENGTH, "4");
+        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_FONT_NAMES, "Arial");
+        properties.setProperty(Constants.KAPTCHA_NOISE_COLOR, "164,128,55");
+        properties.setProperty(Constants.KAPTCHA_OBSCURIFICATOR_IMPL, "com.google.code.kaptcha.impl.ShadowGimpy");
+        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_CHAR_STRING, "023456789abcdefghjkmnpqrstwxyzABCDEFGHJKMNPQRSTWXYZ");
+        Config config = new Config(properties);
+        kaptcha.setConfig(config);
+        return kaptcha;
+    }
+
+}
---
+++ springboot-security/src/main/java/priv/lyq/springboot/security/controller/open/OpenController.java
@@ -0,0 +1,32 @@
+package priv.lyq.springboot.security.controller.open;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.awt.image.BufferedImage;
+
+/**
+ * @author Li Yuqing
+ * @date 2020-07-21 10:01:00
+ */
+@Slf4j
+@RestController
+@RequestMapping(path = "open")
+public class OpenController {
+
+    @Resource(name = "kaptcha")
+    private DefaultKaptcha kaptcha;
+
+    @GetMapping(path = "code", produces = MediaType.IMAGE_JPEG_VALUE)
+    public BufferedImage o1() {
+        String text = kaptcha.createText();
+        log.info("验证码：{}", text);
+        return kaptcha.createImage(text);
+    }
+
+}
