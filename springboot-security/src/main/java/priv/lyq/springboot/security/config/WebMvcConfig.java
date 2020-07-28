eb1cc851469a90e057a1ce898fb7ec9057bf7f3b
kinglyq <755365600@qq.com>
2020-07-28 22:54:00 +0800

    添加WebMvc BufferedImage converters

---
+++ springboot-security/src/main/java/priv/lyq/springboot/security/config/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package priv.lyq.springboot.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.BufferedImageHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * @author Li Yuqing
+ * @date 2020-07-28 15:48:00
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    /**
+     * 添加BufferedImage转换
+     *
+     * @param converters converters
+     */
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new BufferedImageHttpMessageConverter());
+    }
+
+}
