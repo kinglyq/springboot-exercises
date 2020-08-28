70e758e48ad5daffdb79af89bd6a5d7e882143e9
kinglyq <755365600@qq.com>
2020-08-28 22:36:04 +0800

    添加从Authentication获取用户信息

--- springboot-security/src/main/java/top/funsite/springboot/security/controller/UserController.java
+++ springboot-security/src/main/java/top/funsite/springboot/security/controller/UserController.java
@@ -1,8 +1,11 @@
 package top.funsite.springboot.security.controller;
 
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import top.funsite.springboot.security.entity.User;
 
 /**
  * @author Li Yuqing
@@ -12,10 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "user", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
 
-
-    /*@PutMapping("${id}")
-    public ResponseResult updateUser(@PathVariable("id") Integer id, @RequestBody User user) {
-
-    }*/
+    /**
+     * 通过注入 Authentication 对象，进而获取到当前用户信息
+     *
+     * @param authentication {@link Authentication}
+     * @return 用户信息
+     * @see User
+     */
+    @GetMapping("info")
+    public User getUserInfo(Authentication authentication) {
+        return (User) authentication.getPrincipal();
+    }
 
 }
