89aded8bf5705eacd8331ddc9b5ca756d2ac15f3
kinglyq <755365600@qq.com>
2020-08-28 22:38:32 +0800

    添加log输出访问地址

--- springboot-security/src/main/java/top/funsite/springboot/security/SpringbootSecurityApplication.java
+++ springboot-security/src/main/java/top/funsite/springboot/security/SpringbootSecurityApplication.java
@@ -1,19 +1,36 @@
 package top.funsite.springboot.security;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @author Li Yuqing
  * @date 2020-05-24 08:17:00
  */
+@Slf4j
 @SpringBootApplication
 @MapperScan("top.funsite.springboot.security.mapper")
 public class SpringbootSecurityApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringbootSecurityApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext context = SpringApplication.run(SpringbootSecurityApplication.class, args);
+        Environment env = context.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application SPRING SECURITY is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+                "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
+                "----------------------------------------------------------");
+
     }
 
 }
